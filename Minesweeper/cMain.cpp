49fecddd1c6f346f5368b1d06140f14c90c9d8bd
CK-Austin <caustin2@student.fullsail.edu>
2022-05-11 22:08:33 -0400

    Add project files.

---
+++ Minesweeper/cMain.cpp
@@ -0,0 +1,11 @@
+#include "cMain.h"
+
+cMain::cMain() : wxFrame(nullptr, wxID_ANY, "Working Title", wxPoint(30, 30), wxSize(800, 600))
+{
+	m_btn1 = new wxButton(this, wxID_ANY, "Click me", wxPoint(10, 10)
+}
+
+cMain::~cMain()
+{
+
+}
